e32c2881bbf179f7bc386cc29edd09d47eefc46c
Cheddar629 <franciscoyan629@gmail.com>
2023-02-15 17:28:22 -0300

    Add return statement to the function

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function getComputerChoice(){
     console.log("Random number is " + random_num)
     if(random_num === 0){
         choice = "Rock"
+        return choice
     } else if(random_num === 1){
         choice = "Paper"
+        return choice
     } else if(random_num === 2){
         choice = "Scissor"
+        return choice
     }
     console.log("The choice is " + choice)
 }
 getComputerChoice()
+
